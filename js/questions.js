0907583c48e74ae71092fdfc18ba4790e5486b5a
yasin-ai <73063465+yasin-ai@users.noreply.github.com>
2021-02-19 09:48:15 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "The last Mauryan ruler, who was killed by his Commander-in-Chief was __?",
+    answer: "Brihadratha",
+    options: [
+      "Dasaratha",
+      "Kun-ala",
+      "Samprati",
+      "Brihadratha"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which rock edict gives information about Asoka’s conversion to Buddhism?",
+    answer: "Bhabru rock edict",
+    options: [
+      "Bhabru rock edict",
+      "Kalinga rock edict",
+      "Tarai rock edict",
+      "Barabar cave rock edict"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which among the following was the capital of Chera Kingdom?",
+    answer: "Vanji",
+    options: [
+      "Tondi",
+      "Musiri",
+      "Vanji",
+      "Uraiyaur"
+    ]
+  },
+    {
+    numb: 4,
+    question: "In ancient India, which dynasty issued the largest number of Gold coins?",
+    answer: "Guptas",
+    options: [
+      "Mauryas",
+      "Guptas",
+      "Cholas",
+      "Nandas"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following religious text is written in the form of questions and answers?",
+    answer: "Abhidhamma Pitaka",
+    options: [
+      "Vinaya Pitaka",
+      "Sutta Pitaka",
+      "Abhidhamma Pitaka",
+      "Prakirnas"
+    ]
+  }
+];
